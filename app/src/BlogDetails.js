7ce506b9309c05b757e9c9f54f275d9e4cd3529f
Ivan Andriukha <64382696+JackGhoster@users.noreply.github.com>
2023-05-06 21:18:24 +0300

    Finished :)

--- app/src/BlogDetails.js
+++ app/src/BlogDetails.js
@@ -1,10 +1,21 @@
-import { useParams } from 'react-router-dom';
+import { useState } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
 import useFetch from './useFetch';
-
 const BlogDetails = () => {
 	const { id } = useParams();
 	const url = 'http://localhost:8000/blogs/';
 	const { data: blog, error, isFetching } = useFetch(url + id);
+	const history = useHistory();
+	const [isDeleting, setIsDeleting] = useState(false);
+	const handleDelete = () => {
+		setIsDeleting(true); //while deleting
+		fetch(url + blog.id, {
+			method: 'DELETE',
+		}).then(() => {
+			setIsDeleting(true); // on deleted
+			history.push('/');
+		});
+	};
 	return (
 		<div className="blog-details">
 			{/* While loading the blog from the server */}
@@ -25,6 +36,8 @@ const BlogDetails = () => {
 					<h1>{blog.title}</h1>
 					<h2>{blog.body}</h2>
 					<h3>Written by {blog.author}</h3>
+					{!isDeleting && <button onClick={handleDelete}>Delete</button>}
+					{isDeleting && <button disabled>Deleting...</button>}
 				</article>
 			)}
 		</div>
--- app/src/Create.js
+++ app/src/Create.js
@@ -1,7 +1,62 @@
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 const Create = () => {
+	const url = 'http://localhost:8000/blogs';
+
+	const [title, setTitle] = useState('');
+	const [body, setBody] = useState('');
+	const [author, setAuthor] = useState('');
+
+	const [isPosting, setIsPosting] = useState(false);
+
+	const history = useHistory();
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		const blog = { title, body, author };
+		setIsPosting(true);
+		fetch(url, {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(blog),
+		}).then(() => {
+			console.log('posted successfully');
+			setIsPosting(false);
+			history.push('/');
+		});
+	};
+
 	return (
 		<div className="create">
-			<h2>Add new blog</h2>
+			<h2>Post a new blog</h2>
+
+			<form onSubmit={handleSubmit}>
+				<label>Blog Title:</label>
+				<input
+					type="text"
+					placeholder="Enter the title for the blog!"
+					value={title}
+					onChange={(e) => setTitle(e.target.value)}
+				/>
+				<label>Blog Text:</label>
+				<textarea
+					required
+					value={body}
+					onChange={(e) => setBody(e.target.value)}
+				/>
+				<label type="text" required>
+					Blog Author:
+				</label>
+				<input
+					type="text"
+					required
+					placeholder="Enter your name"
+					value={author}
+					onChange={(e) => setAuthor(e.target.value)}
+				/>
+				{!isPosting && <button>Post Blog</button>}
+				{isPosting && <button disabled>Posting...</button>}
+			</form>
 		</div>
 	);
 };
